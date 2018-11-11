6e38b731a52f3ce4d9b1ff1b829c4d2f970c6ba1
Joshua Mitchell <36055284+Jmitch13@users.noreply.github.com>
2018-11-11 10:08:48 -0800

    Update GroceryCheckout.cpp

--- GroceryCheckout.cpp
+++ GroceryCheckout.cpp
@@ -1,5 +1,5 @@
 #include	<iostream>
-#include <stdexcept>
+#include    <stdexcept>
 #include	"GroceryCheckout.h"
 
 using namespace std;
@@ -7,7 +7,13 @@ using namespace std;
 bool GroceryInventory::AddItem(const string& name, int quantity,
 							   float price, bool taxable)
 {
-	//	TO BE COMPLETED.
+    //Checks to see if the item has already been added
+	if (item_.find(name) != item_.end())
+        return false;
+    else
+    //Adds the item if it was not found
+        item_[name] = GroceryItem(name, quantity, price, taxable);
+        return true;
 }
 
 void GroceryInventory::CreateFromFile(const string& fileName)
@@ -44,25 +50,77 @@ void GroceryInventory::CreateFromFile(const string& fileName)
 
 Receipt GroceryInventory::CreateReceipt(const string& fileName)
 {
-	//	TO BE COMPLETED.
+    ifstream   file(fileName);
+    string   name;
+    Receipt   receipt;
+    receipt.subtotal_ = receipt.taxAmount_ = receipt.total_ = 0.0;
+    if (file.is_open())
+    {
+        while (true)
+        {
+            //Checks to see if the file can still keep reading
+            file >> name;
+            if (!file.fail())
+            {
+                //Create a grocery item object
+                GroceryItem* gi = FindItem(name);
+                if(gi != nullptr)
+                {
+                    receipt.item_.push_back(ReceiptItem(name, gi->price_));
+                    //update total
+                    receipt.subtotal_ += gi->price_;
+                    //Checks to see if it is taxable
+                    if(gi->taxable_)
+                    {
+                        receipt.taxAmount_ += taxRate_ / 100.0 * gi->price_;
+                    }
+                    //update inventory
+                    gi->quantity_ --;
+                }
+            }
+            else
+            {
+                break;
+            }
+        }
+        file.close();
+    }
+    else
+    {
+        cout << "Could not open file " + fileName << endl;
+    }
+    //Creates the receipt
+    receipt.total_ = receipt.subtotal_ + receipt.taxAmount_;
+    return receipt;
 }
 
-GroceryItem*	GroceryInventory::FindItem(const string& name)
+GroceryItem* GroceryInventory::FindItem(const string& name)
 {
-	//	TO BE COMPLETED.
+    //Returns a nullptr if the item is not found
+	if (item_.find(name) == item_.end())
+        return nullptr;
+    //Returns the found item
+    else
+        return &(item_[name]); //derefrences
 }
 
 bool GroceryInventory::RemoveItem(const string& name)
 {
-	//	TO BE COMPLETED.
+    //Checks to see if the item exists
+	if (item_.find(name) == item_.end())
+        return false;
+    //Removes the item
+    else
+        item_.erase(name);
+        return true;
 }
-
+//Sets the taxRate_ variable created earlier to equal the taxRate
 void GroceryInventory::SetTaxRate(float taxRate)
 {
-	//	TO BE COMPLETED.
+    taxRate_ = taxRate;
 }
-
+//returns the size of the map
 size_t GroceryInventory::Size()
 {
-	//	TO BE COMPLETED.
+    return item_.size();
 }
